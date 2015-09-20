c07dcf54df623645451815f626d4b04935e1f00a
Anton Yatsenyuk <anton.yatsenyuk@gmail.com>
2015-09-20 15:09:44 +0300

    composite view

--- app/js/apps/contacts/list/list_view.js
+++ app/js/apps/contacts/list/list_view.js
@@ -1,12 +1,15 @@
 ContactManager.module("ContactsApp.List", function (List, ContactManager, Backbone, Mn, $, _) {
   List.Contact = Mn.ItemView.extend({
-    tagName: "li",
+    tagName: "tr",
     template: "#contact-list-item"
   });
 
-  List.Contacts = Mn.CollectionView.extend({
-    tagName: "ul",
+  List.Contacts = Mn.CompositeView.extend({
+    tagName: "table",
+    className: "table table-hover",
+    template: "#contact-list",
     itemView: List.Contact,
-    childView: List.Contact
+    childView: List.Contact,
+    itemViewContainer: "tbody"
   });
 });
